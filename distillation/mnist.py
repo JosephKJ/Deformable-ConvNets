934867912dea9789de9ae1fb6f26809dbf5ebb37
Joseph KJ <josephkj20@gmail.com>
2017-05-29 16:51:18 +0530

    Update

--- distillation/mnist.py
+++ distillation/mnist.py
@@ -1,4 +1,9 @@
 import mxnet as mx
+import gzip
+import struct
+import numpy as np
+
+
 mnist = mx.test_utils.get_mnist()
 
 batch_size = 100
@@ -46,3 +51,31 @@ acc = mx.metric.Accuracy()
 lenet_model.score(test_iter, acc)
 print(acc)
 assert acc.get()[1] > 0.98
+
+
+def get_mnist():
+    """Download and load the MNIST dataset
+    Returns
+    -------
+    dict
+        A dict containing the data
+    """
+    def read_data(label_url, image_url):
+        with gzip.open(mx.test_utils.download(label_url)) as flbl:
+            struct.unpack(">II", flbl.read(8))
+            label = np.fromstring(flbl.read(), dtype=np.int8)
+        with gzip.open(mx.test_utils.download(image_url), 'rb') as fimg:
+            _, _, rows, cols = struct.unpack(">IIII", fimg.read(16))
+            image = np.fromstring(fimg.read(), dtype=np.uint8).reshape(len(label), rows, cols)
+            image = image.reshape(image.shape[0], 1, 28, 28).astype(np.float32)/255
+        return (label, image)
+
+    # changed to mxnet.io for more stable hosting
+    # path = 'http://yann.lecun.com/exdb/mnist/'
+    path = 'http://data.mxnet.io/data/mnist/'
+    (train_lbl, train_img) = read_data(
+        path+'train-labels-idx1-ubyte.gz', path+'train-images-idx3-ubyte.gz')
+    (test_lbl, test_img) = read_data(
+        path+'t10k-labels-idx1-ubyte.gz', path+'t10k-images-idx3-ubyte.gz')
+    return {'train_data':train_img, 'train_label':train_lbl,
+            'test_data':test_img, 'test_label':test_lbl}
