20977fb60b972b2832704b59cd429bd3f4d369a2
Joseph KJ <josephkj20@gmail.com>
2017-06-03 17:45:51 +0530

    Update

--- distillation/cifar-10.py
+++ distillation/cifar-10.py
@@ -52,38 +52,57 @@ eps = 1e-5
 
 
 
+# # Building the network
+# print 'Building Network.'
+# data = mx.sym.Variable('data')
+# # Conv-BN-Relu 1
+# conv1 = mx.sym.Convolution(data=data, kernel=(7,7), num_filter=64, pad=(3, 3), stride=(2, 2))
+# bn_conv1 = mx.symbol.BatchNorm(name='bn_conv1', data=conv1, use_global_stats=True, fix_gamma=False, eps=eps)
+# relu1 = mx.sym.Activation(data=bn_conv1, act_type="relu")
+# # Pool 1
+# pool1 = mx.sym.Pooling(data=relu1, pool_type="avg", kernel=(3,3), stride=(2,2))
+# # Conv-BN-Relu 2
+# conv2 = mx.sym.Convolution(data=pool1, kernel=(1,1), num_filter=256, pad=(0, 0), stride=(1, 1))
+# bn_conv2 = mx.symbol.BatchNorm(name='bn_conv2', data=conv2, use_global_stats=True, fix_gamma=False, eps=eps)
+# relu2 = mx.sym.Activation(data=bn_conv2, act_type="relu")
+# # Conv-BN-Relu 3
+# conv3 = mx.sym.Convolution(data=relu2, kernel=(1,1), num_filter=512, pad=(0, 0), stride=(2, 2))
+# bn_conv3 = mx.symbol.BatchNorm(name='bn_conv3', data=conv3, use_global_stats=True, fix_gamma=False, eps=eps)
+# relu3 = mx.sym.Activation(data=bn_conv3, act_type="relu")
+# # Conv-BN-Relu 4
+# conv4 = mx.sym.Convolution(data=relu3, kernel=(1,1), num_filter=1024, pad=(0, 0), stride=(2, 2))
+# bn_conv4 = mx.symbol.BatchNorm(name='bn_conv4', data=conv4, use_global_stats=True, fix_gamma=False, eps=eps)
+# relu4 = mx.sym.Activation(data=bn_conv4, act_type="relu")
+# # Fully Connected 1
+# flatten = mx.sym.Flatten(data=relu4)
+# fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=10)
+# # Softmax
+# net = mx.sym.SoftmaxOutput(data=fc1, name='softmax')
+
 # Building the network
 print 'Building Network.'
 data = mx.sym.Variable('data')
-# Conv-BN-Relu 1
-conv1 = mx.sym.Convolution(data=data, kernel=(7,7), num_filter=64, pad=(3, 3), stride=(2, 2))
-bn_conv1 = mx.symbol.BatchNorm(name='bn_conv1', data=conv1, use_global_stats=True, fix_gamma=False, eps=eps)
-relu1 = mx.sym.Activation(data=bn_conv1, act_type="relu")
-# Pool 1
-pool1 = mx.sym.Pooling(data=relu1, pool_type="avg", kernel=(3,3), stride=(2,2))
-# Conv-BN-Relu 2
-conv2 = mx.sym.Convolution(data=pool1, kernel=(1,1), num_filter=256, pad=(0, 0), stride=(1, 1))
-bn_conv2 = mx.symbol.BatchNorm(name='bn_conv2', data=conv2, use_global_stats=True, fix_gamma=False, eps=eps)
-relu2 = mx.sym.Activation(data=bn_conv2, act_type="relu")
-# Conv-BN-Relu 3
-conv3 = mx.sym.Convolution(data=relu2, kernel=(1,1), num_filter=512, pad=(0, 0), stride=(2, 2))
-bn_conv3 = mx.symbol.BatchNorm(name='bn_conv3', data=conv3, use_global_stats=True, fix_gamma=False, eps=eps)
-relu3 = mx.sym.Activation(data=bn_conv3, act_type="relu")
-# Conv-BN-Relu 4
-conv4 = mx.sym.Convolution(data=relu3, kernel=(1,1), num_filter=1024, pad=(0, 0), stride=(2, 2))
-bn_conv4 = mx.symbol.BatchNorm(name='bn_conv4', data=conv4, use_global_stats=True, fix_gamma=False, eps=eps)
-relu4 = mx.sym.Activation(data=bn_conv4, act_type="relu")
+# Conv-Relu-Pool 1
+conv1 = mx.sym.Convolution(data=data, kernel=(5,5), num_filter=20)
+relu1 = mx.sym.Activation(data=conv1, act_type="relu")
+pool1 = mx.sym.Pooling(data=relu1, pool_type="max", kernel=(2,2), stride=(2,2))
+# Conv-Relu-Pool 2
+conv2 = mx.sym.Convolution(data=pool1, kernel=(5,5), num_filter=50)
+relu2 = mx.sym.Activation(data=conv2, act_type="relu")
+pool2 = mx.sym.Pooling(data=relu2, pool_type="max", kernel=(2,2), stride=(2,2))
+# Conv-Relu-Pool 3
+conv3 = mx.sym.Convolution(data=pool2, kernel=(1,1), num_filter=50)
+relu3 = mx.sym.Activation(data=conv3, act_type="relu")
 # Fully Connected 1
-flatten = mx.sym.Flatten(data=relu4)
+flatten = mx.sym.Flatten(data=relu3)
 fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=10)
 # Softmax
-net = mx.sym.SoftmaxOutput(data=fc1, name='softmax')
-
+lenet = mx.sym.SoftmaxOutput(data=fc1, name='softmax')
 
 
 print 'Building Module.'
 # Module
-net_module = mx.mod.Module(net, context=mx.gpu(5))
+net_module = mx.mod.Module(lenet, context=mx.gpu(5))
 
 print 'Training.'
 # Train
