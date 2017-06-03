82d3342f64227100071b2885a43551f850a528b9
Joseph KJ <josephkj20@gmail.com>
2017-06-03 15:55:52 +0530

    Update

--- distillation/cifar-10.py
+++ distillation/cifar-10.py
@@ -65,9 +65,8 @@ pool2 = mx.sym.Pooling(data=relu2, pool_type="max", kernel=(2,2), stride=(2,2))
 # Conv-Relu-Pool 3
 conv3 = mx.sym.Convolution(data=pool2, kernel=(5,5), num_filter=50)
 relu3 = mx.sym.Activation(data=conv3, act_type="relu")
-pool3 = mx.sym.Pooling(data=relu3, pool_type="max", kernel=(2,2), stride=(2,2))
 # Fully Connected 1
-flatten = mx.sym.Flatten(data=pool3)
+flatten = mx.sym.Flatten(data=relu3)
 fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=500)
 relu3 = mx.sym.Activation(data=fc1, act_type="relu")
 # Fully Connected 2
