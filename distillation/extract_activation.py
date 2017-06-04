8f93ea904c3216286bab897d74d91e49e4694f55
Joseph KJ <josephkj20@gmail.com>
2017-06-04 10:53:47 +0530

    Update

--- distillation/extract_activation.py
+++ distillation/extract_activation.py
@@ -27,7 +27,8 @@ sys.path.insert(0, os.path.join(curr_path, '../external/mxnet', config.MXNET_VER
 import mxnet as mx
 from function.test_rcnn import test_rcnn
 from utils.create_logger import create_logger
-
+from core.loader import TestLoader
+from utils.load_model import load_param
 
 def main():
     ctx = [mx.gpu(int(i)) for i in config.gpus.split(',')]
@@ -41,6 +42,25 @@ def main():
     imdb = eval(config.dataset.dataset)(config.dataset.test_image_set, config.dataset.root_path, config.dataset.dataset_path, result_path=final_output_path)
     roidb = imdb.gt_roidb()
 
+    # get test data iter
+    test_data = TestLoader(roidb, config, batch_size=len(ctx), shuffle=True, has_rpn=True)
+
+    # load model
+    prefix = os.path.join(final_output_path, '..', '_'.join([iset for iset in config.dataset.image_set.split('+')]), config.TRAIN.model_prefix)
+    arg_params, aux_params = load_param(prefix, config.TEST.test_epoch, process=True)
+
+    # infer shape
+    data_shape_dict = dict(test_data.provide_data_single)
+    sym_instance.infer_shape(data_shape_dict)
+
+    sym_instance.check_parameter_shapes(arg_params, aux_params, data_shape_dict, is_train=False)
+
+    # decide maximum shape
+    data_names = [k[0] for k in test_data.provide_data_single]
+    label_names = None
+    max_data_shape = [[('data', (1, 3, max([v[0] for v in config.SCALES]), max([v[1] for v in config.SCALES])))]]
+
+    print max_data_shape
     print 'done'
 
 
