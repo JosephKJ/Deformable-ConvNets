b824dcc90b878ddb9b154c953b0d2aa17dd73aa2
Joseph KJ <josephkj20@gmail.com>
2017-06-04 10:35:35 +0530

    Update

--- distillation/extract_activation.py
+++ distillation/extract_activation.py
@@ -30,7 +30,9 @@ from utils.create_logger import create_logger
 def main():
     ctx = [mx.gpu(int(i)) for i in config.gpus.split(',')]
     print args
-
+    print '***'
+    print args.cfg
+    print '***'
     logger, final_output_path = create_logger(config.output_path, args.cfg, 'activations')
     print final_output_path
     print ctx
