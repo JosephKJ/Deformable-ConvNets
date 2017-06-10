b5f732d7b131f796b6e233cac34cfa61afcf78ee
Joseph KJ <josephkj20@gmail.com>
2017-06-10 10:13:41 +0530

    Update

--- distillation/metric.py
+++ distillation/metric.py
@@ -9,29 +9,13 @@ def get_names():
 
 class DistillationMetric(mx.metric.EvalMetric):
     def __init__(self):
-        super(DistillationMetric, self).__init__('RPNAcc')
+        super(DistillationMetric, self).__init__('Student_Accuracy')
         self.pred, self.label = get_names()
 
     def update(self, labels, preds):
-        pred = preds[self.pred.index('rpn_cls_prob')]
-        label = labels[self.label.index('rpn_label')]
 
-        # # pred (b, c, p) or (b, c, h, w)
-        # pred_label = mx.ndarray.argmax_channel(pred).asnumpy().astype('int32')
-        # pred_label = pred_label.reshape((pred_label.shape[0], -1))
-        #
-        # # label (b, p)
-        # label = label.asnumpy().astype('int32')
-        #
-        # # filter with keep_inds
-        # keep_inds = np.where(label != -1)
-        # pred_label = pred_label[keep_inds]
-        # label = label[keep_inds]
-        #
-        # self.sum_metric += np.sum(pred_label.flat == label.flat)
-        # self.num_inst += len(pred_label.flat)
+        pred_label = preds[0].asnumpy()
+        label = labels[0].asnumpy()
 
-        print pred
-        print label
-        self.sum_metric += 0
-        self.num_inst += 0
+        self.sum_metric += np.sum(pred_label.flat == label.flat)
+        self.num_inst += len(pred_label.flat)
